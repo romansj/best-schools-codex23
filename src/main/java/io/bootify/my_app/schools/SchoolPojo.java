18775c1d6ff6e67d09c1e399d71a68aeae3583ec
Jānis Romāns <romansj@outlook.com>
2023-06-04 00:27:26 +0300

    school pojo and csv read

---
+++ src/main/java/io/bootify/my_app/schools/SchoolPojo.java
@@ -0,0 +1,20 @@
+package io.bootify.my_app.schools;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public record SchoolPojo(
+        String Id,
+        @JsonAlias("school")
+        String name,
+        String website,
+        String location,
+        String lv_exam,
+        String en_exam,
+        String math_exam,
+        String school_lunches,
+        String activities,
+        String profile_based_learning,
+        String school_type,
+        String is_remote) {
+}
